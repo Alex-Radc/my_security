ea4b92b8048068186a328907dcb26ab6fd51961f
Alex-Radc <mor94ok.ua@gmail.com>
2022-10-07 20:35:53 +0300

    Initial commit

---
+++ src/main/java/com/alexra/spring/securit/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.alexra.spring.securit.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
